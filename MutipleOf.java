3fe3fe4804e06ff06b9d17c4621e3e6896ecdee7
KetkiHatwar <87512358+KetkiHatwar@users.noreply.github.com>
2021-09-28 13:43:30 +0530

    Euler 1st problem

---
+++ MutipleOf.java
@@ -0,0 +1,15 @@
+package com.hcl;
+
+public class MutipleOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+for(int i=1;i<1000;i++) {
+if(i%3==0||i%5==0) {
+	sum=sum+i;
+}	
+}
+	System.out.println(sum);
+	}
+}
